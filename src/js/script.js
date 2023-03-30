1238f5118f38c77c135200fe8ce9d5235ca83b9e
Maksim <lvutinwork@gmail.com>
2023-03-30 19:07:21 +0300

    end

--- src/js/script.js
+++ src/js/script.js
@@ -32,7 +32,74 @@ $(document).ready(function(){
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
             $('.overlay, #order').fadeIn('slow');
         })
-      })
+      });
+
+      function valideForms(form) {
+        $(form).validate({
+          rules: {
+            name: {
+              required: true,
+              minlength: 2
+            },
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: {
+              required: "Пожалуйста, введите своё имя",
+              minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введён адрес почты"
+            }
+          }
+        });
+      };
+
+      valideForms('#consultation-form');
+      valideForms('#consultation form');
+      valideForms('#order form');
+
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+      $('form').submit(function(e) {
+          e.preventDefault();
+          $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+          }).done(function () {
+              $(this).find("input").val("");
+              $('#consultation, #order').fadeOut();
+              $('.overlay, #thanks').fadeIn('slow');
+
+              $('form').trigger('reset');
+          });
+          return false;
+      });
+
+
+      // Smooth scroll and pageup
+      $(window).scroll(function() {
+          if ($(this).scrollTop() > 1600) {
+              $('.pageup').fadeIn();
+          } else {
+            $('.pageup').fadeOut();
+          }
+      });
+
+      $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
   });
 
 
